2311341f1dcc99631fe438662290b26a77ffc078
Shehab Chowdhury <shehabc58@gmail.com>
2023-09-20 20:19:17 -0400

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "Shehab",
+    streetAddress: "123 Liberty"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee}
+    newEmployee[key]= value
+    return newEmployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const removeEmployee = {...employee}
+    removeEmployee[key] = undefined
+    return removeEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    employee[key] = undefined
+    return employee
+}
